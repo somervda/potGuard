b2be703f0036283e49d3287e6fb7e96a4c3ad65d
David <39265971+somervda@users.noreply.github.com>
2023-03-02 16:50:04 -0500

    first commit

---
+++ dasserver.py
@@ -0,0 +1,43 @@
+import uasyncio
+import sys
+from microdot_asyncio import Microdot
+
+import network
+import time
+
+ssid = 'guest24'
+password = 'backyard'
+
+wlan = network.WLAN(network.STA_IF)
+app = Microdot()
+
+
+@app.route('/')
+async def hello(request):
+    return 'Hello world'
+
+
+def print_hello():
+    print("Hello")
+
+
+def start_server():
+
+    try:
+        # Connect to WLAN
+        wlan.active(True)
+        wlan.connect(ssid, password)
+        print("Connecting:")
+        while not wlan.isconnected() and wlan.status() >= 0:
+            print(".", end="")
+            time.sleep(1)
+
+        print("Connected! IP Address = " + wlan.ifconfig()[0])
+        # setup webserver
+        print('Starting microdot app')
+        app.run(port=80)
+    except:
+        app.shutdown()
+        wlan.disconnect()
+        print("server shutdown. Exiting...")
+        sys.exit(0)
